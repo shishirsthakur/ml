241a4fd4da412473d33376871ff71d7d2e5cc0e0
shishirsthakur <shishirth4kur@gmail.com>
2025-08-14 23:37:22 +0530

    Problem set 2

---
+++ code/python/PS/PS2/Q1.py
@@ -0,0 +1,7 @@
+number = int(input("Enter the number: "))
+z = int(input("Enter the value of z: "))
+
+
+remainder = number % z
+
+print("The remainder when", number, "is divided by", z, "is:", remainder)
